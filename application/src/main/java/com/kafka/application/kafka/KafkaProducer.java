52f278e32f41d742e4ae13638364aa31ea56cb4d
Aneesh <aneesh@sky.com>
2025-05-20 15:26:26 +1000

    Create Kafka Producer (6)

---
+++ application/src/main/java/com/kafka/application/kafka/KafkaProducer.java
@@ -0,0 +1,25 @@
+package com.kafka.application.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+
+@Service
+public class KafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    private void sendMessage(String message){
+        LOGGER.info(String.format("Message Sent: %s", message));
+        kafkaTemplate.send("MyTopic", message);
+    }
+
+}
