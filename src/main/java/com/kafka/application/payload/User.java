2af1c8e4bd1ebe1c2725892799b685655a3123d4
Aneesh <aneesh@sky.com>
2025-05-23 20:59:46 +1000

    Add default constructor for user

--- src/main/java/com/kafka/application/payload/User.java
+++ src/main/java/com/kafka/application/payload/User.java
@@ -2,15 +2,18 @@ package com.kafka.application.payload;
 
 public class User {
 
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
     public User(int id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
-    private int id;
-    private String firstName;
-    private String lastName;
 
     public int getId() {
         return id;
